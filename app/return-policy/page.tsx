bd0496be39e9b1471399ce61d01a5e415ae92374
AnmolJaiswal-7 <anmoljaiswal380@gmail.com>
2025-05-04 16:52:06 +0530

    font updated

---
+++ app/return-policy/page.tsx
@@ -0,0 +1,139 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div className="page-wrapper">
+      <div className="policy-wrap">
+        <div className="container w-container">
+          <h4>Introduction</h4>
+          <p>
+            Cras auctor nisl non ornare pellentesque orci leo congue et vel
+            pellentesque at amet quisque arcu purus tincidunt in lectus
+            adipiscing phasellus porta sapien adipiscing at enim non mattis
+            condimentum et sit aliquam fames convallis semper pharetra at neque
+            ac dignissim.
+            <br />‍
+          </p>
+          <h4>Cancellation &amp; Refund</h4>
+          <div className="check-list">
+            <p>
+              A. Cras auctor nisl non ornare pellentesque orci leo congue et vel
+              pellentesque at amet quisque arcu purus tincidunt in lectus
+              adipiscing phasellus
+            </p>
+            <ul role="list" className="rp-check-list w-list-unstyled">
+              <li>
+                Erat in vitae pretium adipiscing convallis auctor lacus
+                pharetra.
+              </li>
+              <li>Orci arcu nisi nisl quisque fermentum vulputate vitae.</li>
+              <li>
+                Cursus aliquet nunc faucibus mollis sed fusce fames consequat.
+              </li>
+            </ul>
+          </div>
+          <div className="check-list">
+            <p>B. Pellentesque lacus tellus et lobortis dignissim molestie.</p>
+            <ul role="list" className="rp-check-list w-list-unstyled">
+              <li>Sit urna elit nunc ut enim lobortis odio.</li>
+              <li>Vel ac eget purus tincidunt quis mauris.</li>
+              <li>Luctus rhoncus posuere ultrices ipsum tristique.</li>
+              <li>
+                Duis tincidunt pellentesque tellus et tempus mauris neque.
+              </li>
+              <li>Tellus sit arcu risus dis egestas ut ultrices gravida.</li>
+              <li>Auctor laoreet imperdiet proin et.er id.</li>
+            </ul>
+          </div>
+          <div className="check-list">
+            <p>C. Orci morbi ut potenti enim morbi sagittis viverra lacus.</p>
+            <ul role="list" className="rp-check-list w-list-unstyled">
+              <li>
+                Vitae risus vel et purus nunc adipiscing amet pellentesque sed
+                id imperdiet elit a.
+              </li>
+              <li>
+                Imperdiet aliquet sit mattis elit sit risus ultricies odio quam
+                at arcu.
+              </li>
+              <li>Pellentesque nibh fringilla cursus morbi egestas dictum.</li>
+              <li>
+                Feugiat elit nullam turpis bibendum nec et tincidunt tempor
+                praesent malesuada sodales amet et nunc in nibh elit.
+              </li>
+            </ul>
+          </div>
+          <h4>Refund Owing to Cancellation</h4>
+          <p>
+            At senectus velit feugiat tortor est volutpat hendrerit orci. Tortor
+            adipiscing proin orci euismod accumsan cursus felis morbi. Non
+            dignissim sapien nec porttitor tellus. Eget feugiat sed sapien et.
+            Faucibus sollicitudin tortor vestibulum tellus neque in turpis.
+            Massa ipsum felis fermentum tortor mattis fermentum massa. Diam
+            magna pharetra ipsum porta commodo tristique quam. Odio cras sed dis
+            molestie vehicula amet. Non libero tortor tellus viverra viverra
+            nulla amet aliquet. Dignissim aliquet magna vivamus et ac
+            pellentesque. Nulla accumsan turpis vel convallis. Tellus pulvinar
+            diam ultricies sed id sed aenean. Est enim commodo pharetra diam.
+            Convallis leo neque faucibus et convallis vitae rhoncus.
+          </p>
+          <p>
+            Odio porttitor eget ligula massa in risus fermentum fermentum enim.
+            Feugiat euismod turpis sit massa sem pellentesque. Dapibus viverra
+            in et vestibulum vulputate adipiscing lobortis rhoncus gravida. Leo
+            quam cursus sit at amet augue ut et ut. Semper leo viverra in neque
+            amet consectetur. Sagittis enim arcu lacus a feugiat in suspendisse.
+            Eget non dui ultrices viverra sociis tortor. Lorem ullamcorper
+            dignissim sed molestie duis cursus. Amet egestas enim ornare elit
+            nascetur lectus porttitor.
+            <br />‍
+          </p>
+          <h4>Returns &amp; Refunds</h4>
+          <p>
+            Ut sem orci vivamus velit elit rutrum eget. Nulla ut malesuada fames
+            amet volutpat eros pellentesque blandit risus. Commodo nullam vel
+            vel consectetur. Pellentesque convallis viverra interdum elit
+            blandit ut tristique pellentesque. Egestas nibh sed laoreet
+            consectetur donec. Ultricies duis proin aliquam eget vivamus cursus.
+            Rhoncus felis senectus vestibulum consectetur ac. Sed neque neque
+            odio morbi. Pulvinar sed ut sed egestas malesuada consequat nec.
+          </p>
+          <p>
+            A vitae malesuada sem feugiat. Nisl dictum vulputate massa tristique
+            tempus. Ultrices porttitor vitae elementum ullamcorper viverra.
+            Porta tincidunt nunc dui felis sit ut tellus risus. Pellentesque
+            morbi adipiscing suspendisse adipiscing vitae arcu blandit egestas
+            volutpat. Risus eget pellentesque amet sed dictum odio.
+            <br />‍
+          </p>
+          <h4>Item Damaged During Shipment</h4>
+          <p>
+            Id lectus dictumst sodales donec sed ullamcorper tellus. Morbi enim
+            in faucibus ultrices cras. Senectus ac fames at nunc neque nisi
+            hendrerit cursus. Purus ultrices ipsum aenean leo pretium accumsan.
+            Est odio mattis risus diam. Amet sit sit tortor neque magna mi
+            tristique non vulputate. Blandit tincidunt pharetra at sollicitudin
+            proin nisl molestie dictumst fermentum. Enim dolor integer justo
+            dignissim consectetur diam elit sed neque. Enim vitae erat pretium
+            praesent cras aliquet nisl senectus adipiscing. Enim sed fames quam
+            ullamcorper eleifend ut phasellus. Fermentum hendrerit aenean
+            habitant justo. Egestas malesuada tortor arcu mauris faucibus
+            facilisi.
+          </p>
+          <p>
+            Sapien eget morbi amet sagittis integer tortor ornare quam.
+            Ullamcorper feugiat eu aliquet sed amet turpis arcu vel integer.
+            Nulla turpis pharetra sit pellentesque euismod sed diam. Pharetra
+            turpis nibh in elit ut. Cras nulla tortor fames risus vitae. Eget
+            ipsum sed ipsum egestas dui nulla lacus ac. Sit amet sociis
+            adipiscing sed nisl. Nisl a tortor arcu dui sit vivamus viverra.
+            Aliquet aliquet etiam rutrum elementum sapien. Lacus aliquam
+            fermentum ut bibendum imperdiet quam quis euismod.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default page;
