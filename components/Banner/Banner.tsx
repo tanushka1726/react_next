bd1054c9aa5547cb1315dc9cf01e8b73e282a43f
AnmolJaiswal-7 <anmoljaiswal380@gmail.com>
2025-05-04 17:50:10 +0530

    banner type error updated

--- components/Banner/Banner.tsx
+++ components/Banner/Banner.tsx
@@ -1,15 +1,18 @@
 import React from "react";
-import "./Banner.css"
-const page = ({title,description}) => {
+import "./Banner.css";
+
+type BannerProps = {
+  title: string;
+  description: string;
+};
+
+const Page = ({ title, description }: BannerProps) => {
   return (
-    <>
     <div className="about-us-head">
       <h1>{title}</h1>
       <p>{description}</p>
-      
     </div>
-    </>
-    
   );
 };
-export default page;
+
+export default Page;
