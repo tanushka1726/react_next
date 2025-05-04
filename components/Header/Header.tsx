3d82cafb0b0706a498cc72401608f27646920948
AnmolJaiswal-7 <anmoljaiswal380@gmail.com>
2025-05-04 23:54:10 +0530

    Header updated for desktop

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,23 +1,66 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import "./Header.css";
 import { IoSearchOutline } from "react-icons/io5";
 import { IoBag } from "react-icons/io5";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Link from "next/link";
+import { IoIosArrowDown, IoIosArrowUp } from "react-icons/io";
+
 const page = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleDropdown = () => setIsOpen(!isOpen);
   return (
-    <>
-      <div className="left-header-elements">
-       <a href="/"> <span> READSTER</span></a>
-        <a href="/books">Books</a>
+    <div style={{ borderBottom: "1px solid black" }}>
+      <div className="left-header-elements r_container">
+        <a href="/">
+          <img src="/header_images/logo.svg" alt="" />
+        </a>
+        {/* <a href="/books">Books</a> */}
+        <div className="dropdown" onClick={toggleDropdown}>
+          <button
+            className="dropdown-toggle btn_books"
+            data-bs-toggle="dropdown "
+            aria-expanded={isOpen}
+            style={{ cursor: "pointer", userSelect: "none" }}
+          >
+            Books {isOpen ? <IoIosArrowUp size={20}/> : <IoIosArrowDown size={20}/>}
+          </button>
+          <ul className={`dropdown-menu dd_menu ${isOpen ? "show" : ""}`} style={{padding: "10px 0", borderRadius: "0", width:"200px" }}>
+            <li>
+              <Link className="dropdown-item" href="/books/self-help">
+                Self Help
+              </Link>
+            </li>
+            <li>
+              <Link className="dropdown-item" href="/books/biography">
+                Biography
+              </Link>
+            </li>
+            <li>
+              <Link className="dropdown-item" href="/books/novel">
+                Novel
+              </Link>
+            </li>
+          </ul>
+        </div>
+
         <a href="/shop">Shop</a>
         <a href="/about">About</a>
         <a href="/blog">Blog</a>
         <a href="">Contact us</a>
         <div className="right-header-elements">
-          <div className="search-bar"><IoSearchOutline />Search</div>
-          <div className="header-cart-icon"><IoBag /></div>
+          <div className="search-bar">
+            <IoSearchOutline size={25} />
+            <input type="text" placeholder="Search book.." />
+          </div>
+          <div className="header-cart-icon">
+            <IoBag />
+          </div>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 export default page;
