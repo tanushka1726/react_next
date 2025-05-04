36f8bb41398be70d08a2def60e4f4f3dfcf61866
AnmolJaiswal-7 <anmoljaiswal380@gmail.com>
2025-05-04 17:21:06 +0530

    fixed root css

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ const page = () => {
       <div className="first-section">
         <div className="first-section-left">
           <div className="first-section-explore">
-          <p>Find your next great <br /> read at our online <br /> book store</p>
+          <h1>Find your next great <br /> read at our online <br /> book store</h1>
           <button type="button" className="btn">Explore Books<IoMdArrowForward /></button>
           </div>
         </div>
