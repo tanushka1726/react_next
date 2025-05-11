afbf96bd8e114db4c3735b646bbfbcc703a01c8b
AnmolJaiswal-7 <anmoljaiswal380@gmail.com>
2025-05-11 19:11:40 +0530

    updated layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
+import Header from "@/components/Header/Header";
+import Footer from "@/components/Footer/Footer";
 
 
 const geistSans = Geist({
@@ -27,7 +28,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        <Header />
         {children}
+        <Footer />
       </body>
     </html>
   );
