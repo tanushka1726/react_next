70a3cd7b9df3c01bb5ab38a347e29d05c84402f3
AnmolJaiswal-7 <anmoljaiswal380@gmail.com>
2025-05-11 19:08:45 +0530

    updated footer

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,40 +1,79 @@
 import React from "react";
-import"./Footer.css"
+import "./Footer.css";
+import Image from "next/image";
+import { FaArrowRightLong } from "react-icons/fa6";
+import Link from "next/link";
+import { FaFacebookF, FaInstagram, FaYoutube } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
+type SocialIcon = {
+  icon: React.ElementType;
+  url: string;
+};
+
+const social_icons: SocialIcon[] = [
+  { icon: FaFacebookF, url: "https://facebook.com" },
+  { icon: FaInstagram, url: "https://instagram.com" },
+  { icon: FaYoutube, url: "https://youtube.com" },
+  { icon: FaXTwitter, url: "https://twitter.com" },
+];
+
 const page = () => {
   return (
     <>
-    <div className="footer-section">
-        <div className="left-footer-section">
-            <span>Readster</span>
-            <p>Start Your Empowering Reading</p>
-           <p>Adventure</p> 
-           <button type="button" className="btn btn-light">Contact us</button>
-        </div>
-        <div className="right-footer-section">
-            <div className="basic-right-section">
-            <p>Home</p>
-            <p>About</p>
-            <p>Shop</p>
-            <p>Review</p>
-            <p>Blog</p>
-            <p>Category</p>
-            </div>
-            <div className="privacy-right-section">
-                <p>Style guide</p>
-                <p>Licensing</p>
-                <p>Changelog</p>
-                <p>Error 404</p>
-                <p>Password</p>
-                <p>Privacy Policy</p>
-                <p>Return Policy</p>
-                <p>Coming Soon</p>
-            </div>
-            
-        </div>
-    </div>
-    
-    </>
+      <div className="footer-section">
+        <div className="r_container ">
+          <div className="footer_content">
+            <div className="left-footer-section">
+              <Image
+                src="/header_images/logo.svg"
+                alt="logo"
+                width={139}
+                height={21}
+              />
+              <h2 className="footer_heading">
+                Start Your Empowering Reading Adventure
+              </h2>
 
-  )
+              <button type="button" className="contact_btn">
+                Contact us
+                <i>
+                  <FaArrowRightLong />
+                </i>
+              </button>
+            </div>
+            <div className="right-footer-section">
+              <div className="basic-right-section">
+                <Link href="/">Home</Link>
+                <Link href="/">About</Link>
+                <Link href="/">Shop</Link>
+                <Link href="/">Review</Link>
+                <Link href="/">Blog</Link>
+                <Link href="/">Category</Link>
+              </div>
+              <div className="privacy-right-section">
+                <Link href="/">Privacy Policy</Link>
+                <Link href="/">Return Policy</Link>
+              </div>
+            </div>
+          </div>
+          <div className="copyright">
+            <p>
+              Designed By <span>DEVZI</span>.
+            </p>
+            <div className="social_icons">
+              {social_icons.map((social, index) => {
+                const Icon = social.icon;
+                return (
+                  <Link href={social.url} key={index}>
+                    <Icon />
+                  </Link>
+                );
+              })}
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 export default page;
